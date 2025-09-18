ae6521514755b44120d481d63747b2f5d4f09b03
Danonino <limlimstar32@gmail.com>
2025-09-18 10:46:02 -0600

    Crear index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+console.log('Iniciando...')
+
+import logoutCommand from './commands/logout.js';
+import promoteCommand from './commands/promote.js';
+import demoteCommand from './commands/demote.js';
+import addVideoCommand from './commands/addvideo.js';
+import videoListCommand from './commands/videolist.js';
+import setCategoryCommand from './commands/setcategoria.js';
+import cumCommand from './commands/cum.js';
+import analCommand from './commands/anal.js';
+import qrMainCommand from './commands/qrmain.js';
+import codePremCommand from './commands/codeprem.js';
+import codeCommand from './commands/code.js';
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
+import cfonts from 'cfonts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+
+cfonts.say('SHINOBU', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+
+cfonts.say(`Powered by MDlG-Team`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+
+let isRunning = false
+
+async function start(files) {
+  if (isRunning) return
+  isRunning = true
+
+  for (const file of files) {
+    const args = [join(__dirname, file), ...process.argv.slice(2)]
+
+    setupMaster({
+      exec: args[0],
+      args: args.slice(1),
+    })
+
+    let p = fork()
+
+    p.on('exit', (code) => {
+      isRunning = false
+      start(files)
+
+      if (code === 0) return
+      watchFile(args[0], () => {
+        unwatchFile(args[0])
+        start(files)
+      })
+    })
+  }
+}
+
+start(['starlights.js'])
