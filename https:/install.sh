bb0a5276566e97fa8f2a60472de15e15483cd1a8
Danonino <limlimstar32@gmail.com>
2025-09-19 01:39:39 -0600

    Actualizar install.sh

--- https:/install.sh
+++ https:/install.sh
@@ -1,1 +1,18 @@
+bash
+#!/data/data/com.termux/files/usr/bin/bash
 
+cd && termux-setup-storage
+
+apt-get update -y && apt-get upgrade -y
+
+pkg install -y git nodejs ffmpeg imagemagick
+
+git clone https://github.com/kobDanonino/SHINOBU-KOCHO
+
+cd SHINOBU-KOCHO
+
+npm install
+
+npm update
+
+npm start
