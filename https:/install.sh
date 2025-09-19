81187775362143bf7246446b08731bb88b85a3de
Danonino <limlimstar32@gmail.com>
2025-09-19 11:46:29 -0600

    Actualizar install.sh

--- https:/install.sh
+++ https:/install.sh
@@ -1,18 +1,26 @@
 bash
 #!/data/data/com.termux/files/usr/bin/bash
 
-cd && termux-setup-storage
+Pedir permisos de almacenamiento
+termux-setup-storage
 
-apt-get update -y && apt-get upgrade -y
+Actualizar paquetes
+pkg update -y && pkg upgrade -y
 
+Instalar dependencias necesarias
 pkg install -y git nodejs ffmpeg imagemagick
 
+Clonar el repositorio del bot
 git clone https://github.com/kobDanonino/SHINOBU-KOCHO
 
+Entrar al directorio del bot
 cd SHINOBU-KOCHO
 
+Instalar módulos necesarios
 npm install
 
+(Opcional) Actualizar dependencias si es necesario
 npm update
 
+Iniciar el bot
 npm start
